bfd434e24a5fe5820c94bd4e7cdc55068e78d119
Iyad Kuwatly <ikuwatly@gmail.com>
2017-11-11 10:00:22 -0500

    Add create contact api call

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
 
   render() {
     return (
@@ -30,7 +37,14 @@ class App extends Component {
             onDeleteContact={this.removeContact}
           />
         }/>
-        <Route path="/create" component={CreateContact}/>
+        <Route path="/create" render={( { history } ) =>
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        }/>
       </div>
     )
   }
